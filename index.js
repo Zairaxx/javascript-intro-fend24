c6813b2f9566c6ba74bf83d3122ea9d94b19a468
Zairaxx <brandontsegai@gmail.com>
2024-10-28 10:59:17 +0100

    added comments

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
-let database = [
-    {
-        name:"Frans",
-        age: 29,
-        email: "frans@nackademin.se",
-        phone: "070-453 82 53"
-    },
-    {
-        name:"Hans",
-        age: 44,
-        email: "frans@nackademin.se",
-        phone: "070-453 82 53"
-    },
-    {
-        name:"Olivia",
-        age: 33,
-        email: "frans@nackademin.se",
-        phone: "070-453 82 53"
-    }
-]
+// Variable
+
+// let age = 30;
+
+// const age2 = 60;
+
+// Datatyper
+
+//Strings
+let myName = "Brandon";
+const school = 'Nackademin';
+
+// school = "Ankademin"; 
+
+console.log(school);
+
+//Numbers 
+let age = 30;
+let price = 19.99;
+
+//Booleans
+
+let isWinter = false;
+let isStudent = true;
 
 
-let btn = document.querySelector("#get-employees");
 
-btn.addEventListener("click", () => {
-    let list = document.querySelector("#employee-list");
+// Exempel på ett uttryck dvs ett påstående som returnerar en boolean;
 
-    database.forEach(employee => {
-        let li = document.createElement("li");
-        li.innerText = employee.name + "- " + employee.age + " år";
-        list.append(li);
-    })
-})
+console.log( 0 === false);
+
+// Null & Undefined
+
+//Deklarerar en variabel
+ let finnsInte;
+
+
+//Array - List of values
+
+let myNumbers = [1,2,3,4,5,6,7,8,9,10]
+let myClassmates = ["Daniel", "Kamyab", "Båmi"];
+
+//Can include any type of values, arrays or objects
+let randomArray = [11256346, "Brandon", false, null, [0,1,2], {test: "Hello"} ]
+console.log(randomArray.length);
+
+//Assigning new value to index 1
+randomArray[1] = "Brandom";
+
+console.log(randomArray);
+//Objekt
+
+let myObject = {
+    //key:value
+    name: "Ofelia",
+    age: 22
+}
+
+console.log("Hej, mitt namn är " + myObject.name + " och jag är " + myObject.age + " år gammal.")
+
+console.log(1542352362363262363214483647)
+//Expressions
+
+
+//Funktion
